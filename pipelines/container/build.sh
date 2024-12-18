045595bfa762e198aca8543b1b993d9a94f78aa3
Ahmed Hassan <ahmedh@mathworks.com>
2024-12-17 21:25:56 -0500

    test 5

--- pipelines/container/build.sh
+++ pipelines/container/build.sh
@@ -61,4 +61,25 @@ docker run --init -it --entrypoint /bin/bash --name matlab_padv11 matlab_padv:R2
 sudo docker run --init -it --entrypoint /bin/bash \
         -v /home/ahmedh/data:/home/matlab/data \
         --name matlab_padv2 matlab_padv:R2024b
-opt
+
+# Linux container       
+docker run --init -it \
+        -v /home/ahmedh/artifactory:/home/ahmedh/artifactory \
+        -v /home/ahmedh/project/derived/licenses:/opt/matlab/R2024b/licenses \
+        --entrypoint /bin/bash \
+        --name matlab_padv matlab_padv:R2024b
+
+/usr/bin/docker create --name 3a44960a402f40d08e0e34aa78717c8f_localhost5000matlab_padvR2024b_aa18cf \
+        --label 86052a --workdir /__w/ArtifactMgr/ArtifactMgr  \
+        -e "HOME=/home/matlab" -e GITHUB_ACTIONS=true -e CI=true -v "/var/run/docker.sock":"/var/run/docker.sock" \
+        -v "/home/ahmedh/project/derived/licenses":"/opt/matlab/R2024b/licenses" -v "/home/ahmedh/artifactory":"/home/ahmedh/artifactory" \
+        -v "/home/ahmedh/gh/lingh1/_work":"/__w" -v "/home/ahmedh/gh/lingh1/externals":"/__e":ro -v "/home/ahmedh/gh/lingh1/_work/_temp":"/__w/_temp" \
+        -v "/home/ahmedh/gh/lingh1/_work/_actions":"/__w/_actions" -v "/home/ahmedh/gh/lingh1/_work/_tool":"/__w/_tool" \
+        -v "/home/ahmedh/gh/lingh1/_work/_temp/_github_home":"/github/home" -v "/home/ahmedh/gh/lingh1/_work/_temp/_github_workflow":"/github/workflow" \
+        --entrypoint "tail" localhost:5000/matlab_padv:R2024b "-f" "/dev/null"
+
+/usr/bin/docker start 049299ba0c09d2636b3dc13c5a6635ad0c0431b50f8c322cfec4a7d269a5471c
+/usr/bin/docker ps --all --filter id=049299ba0c09d2636b3dc13c5a6635ad0c0431b50f8c322cfec4a7d269a5471c --filter status=running --no-trunc --format "{{.ID}} {{.Status}}"
+/usr/bin/docker inspect --format "{{range .Config.Env}}{{println .}}{{end}}" 049299ba0c09d2636b3dc13c5a6635ad0c0431b50f8c322cfec4a7d269a5471c
+
+docker exec -it 049299ba0c09d2636b3dc13c5a6635ad0c0431b50f8c322cfec4a7d269a5471c /bin/bash
