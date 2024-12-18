10360dbe891ef6e5349a2aa7ffb9bb5386ca698f
Ahmed Hassan <ahmedh@mathworks.com>
2024-12-18 13:45:21 -0500

    Updates

--- pipelines/scripts/dag/Pipeline.py
+++ pipelines/scripts/dag/Pipeline.py
@@ -37,21 +37,21 @@ class Pipeline:
         self.IMAGE_TAG = self._pipelineObject.get(self._IMAGE_TAG_FIELD)
         self.IMAGE_ARGS = self._pipelineObject.get(self._IMAGE_ARGS_FIELD)
                 
-        self.CONTINUE_ON_ERROR = Utils.getBoolean(pipelineObject, self._pipelineObject.get(self._CONTINUE_ON_ERROR_FIELD), False)
-        self.SUBMODULES_MODE = Utils.getBoolean(pipelineObject, self._pipelineObject.get(self._SUBMODULES_MODE_FIELD), False)
-        self.USE_MATLAB_PLUGIN = Utils.getBoolean(pipelineObject, self._pipelineObject.get(self._USE_MATLAB_PLUGIN_FIELD), False)
-        self.IncrementalPipelineEnabled = Utils.getBoolean(pipelineObject, self._pipelineObject.get(self._INCREMENTAL_PIPELINE_ENABLED_FIELD), True)
+        self.CONTINUE_ON_ERROR = Utils.getBoolean(pipelineObject, self._CONTINUE_ON_ERROR_FIELD, False)
+        self.SUBMODULES_MODE = Utils.getBoolean(pipelineObject, self._SUBMODULES_MODE_FIELD, False)
+        self.USE_MATLAB_PLUGIN = Utils.getBoolean(pipelineObject, self._USE_MATLAB_PLUGIN_FIELD, False)
+        self.IncrementalPipelineEnabled = Utils.getBoolean(pipelineObject, self._INCREMENTAL_PIPELINE_ENABLED_FIELD, True)
         self.MatlabInstrallationPath = self._pipelineObject.get(self._MATLAB_INSTALLATION_PATH_FIELD)
         self.MatlabLaunchCmd = self._pipelineObject.get(self._MATLAB_LAUNCH_CMD_FIELD)
         self.MatlabStartupOptions = self._pipelineObject.get(self._MATLAB_STARTUP_OPTIONS_FIELD)
-        self.AddBatchStartupOption = Utils.getBoolean(pipelineObject, self._pipelineObject.get(self._ADD_BATCH_STARTUP_OPTIONS_FIELD), True)
+        self.AddBatchStartupOption = Utils.getBoolean(pipelineObject, self._ADD_BATCH_STARTUP_OPTIONS_FIELD, True)
         
         self.ProcessName = self._pipelineObject.get(self._PROCESS_NAME_FIELD)
-        self.GenerateReport = Utils.getBoolean(pipelineObject, self._pipelineObject[self._GENERATE_REPORT_FIELD], True)
+        self.GenerateReport = Utils.getBoolean(pipelineObject, self._GENERATE_REPORT_FIELD, True)
         self.ReportPath = self._pipelineObject.get(self._REPORT_PATH_FIELD)
         self.ReportFormat = self._pipelineObject.get(self._REPORT_FORMAT_FIELD)
 
-        self.EnableArtifactCollection = Utils.getBoolean(pipelineObject, self._pipelineObject[self._ENABLE_ARTIFACTS_COLLECTION_FIELD], True)
+        self.EnableArtifactCollection = Utils.getBoolean(pipelineObject, self._ENABLE_ARTIFACTS_COLLECTION_FIELD, True)
         
         self.RunprocessOptions = Utils.getDic(pipelineObject, self._RUN_PROCESS_OPTIONS_DIC, {})
         self.OutputsPaths = None
