20dd1dff029058c34f8b536cbca1524654c42d01
Ahmed Hassan <ahmedh@mathworks.com>
2025-01-24 19:47:00 -0500

    Add the auto gen

---
+++ pipelines/generate_jenkins_pipeline.m
@@ -0,0 +1,13 @@
+function generate_jenkins_pipeline()
+    cp = openProject(pwd);
+
+    op = padv.pipeline.JenkinsOptions;
+    op.PipelineArchitecture = "IndependentModelParallelJobs";
+    op.GeneratedPipelineDirectory = fullfile(cp.RootFolder, "pipelines", "derived");
+    op.MatlabInstrallationPath = "D:/sb/Bslcicd_1217/matlab/bin";
+    op.AgentLabel = "padv_win_agents";
+    op.StopOnStageFailure = true;
+    op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
+    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
+    padv.pipeline.generatePipeline(op, processName = "CIPipeline");
+end
