c21682ebf738808f302b67c8450d69f93fabd00e
Ahmed Hassan <ahmedh@mathworks.com>
2025-01-24 21:41:58 -0500

    Test single job

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -2,7 +2,8 @@ function generate_jenkins_pipeline()
     cp = openProject('scm');
 
     op = padv.pipeline.JenkinsOptions;
-    op.PipelineArchitecture = "IndependentModelParallelJobs";
+    % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
+    op.PipelineArchitecture = "SingleJob";
     op.GeneratedPipelineDirectory = fullfile(cp.RootFolder, "pipelines", "derived");
     op.MatlabInstrallationPath = "D:/sb/Bslcicd_1217/matlab/bin";
     op.AgentLabel = "padv_win_agents";
