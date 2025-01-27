2f5c4cc96e0a494be239b7cef403374a35c3ba9e
Ahmed Hassan <ahmedh@mathworks.com>
2025-01-27 09:00:32 -0500

    Generate files at derived folder first

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -4,11 +4,12 @@ function generate_jenkins_pipeline()
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
     op.PipelineArchitecture = "SerialJobsGroupPerTask";
-    op.GeneratedPipelineDirectory = fileparts(cp.RootFolder);  % Generate the pipeline and dag files just outside the project folder 
     op.MatlabInstallationLocation = "D:/sb/Bslcicd_1217/matlab/bin";
     op.AgentLabel = "padv_win_agents";
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
     op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
     padv.pipeline.generatePipeline(op, "CIPipeline");
+    % Copy the pipeline and dag files just outside the project folder
+    copyfile( fullfile(cp.RootFolder, 'derived', 'pipeline' , '*.json'), fileparts(cp.RootFolder));
 end
