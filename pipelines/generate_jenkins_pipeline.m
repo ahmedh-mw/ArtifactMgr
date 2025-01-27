efbe7064a3c6c3e491efb96ee4842a4b68b476bd
Ahmed Hassan <ahmedh@mathworks.com>
2025-01-27 01:29:32 -0500

    Upload

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -4,7 +4,7 @@ function generate_jenkins_pipeline()
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
     op.PipelineArchitecture = "SerialJobsGroupPerTask";
-    op.GeneratedPipelineDirectory = fullfile(cp.RootFolder, "pipelines", "derived");
+    op.GeneratedPipelineDirectory = fileparts(cp.RootFolder);  % Generate the pipeline and dag files just outside the project folder 
     op.MatlabInstallationLocation = "D:/sb/Bslcicd_1217/matlab/bin";
     op.AgentLabel = "padv_win_agents";
     op.StopOnStageFailure = true;
