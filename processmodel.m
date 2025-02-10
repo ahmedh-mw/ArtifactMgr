c2f39a134f4b123f4d9db9f64d138975fe59426f
Ahmed Hassan <ahmedh@mathworks.com>
2025-02-09 21:05:15 -0500

    Update the MaxProcessTime

--- processmodel.m
+++ processmodel.m
@@ -75,6 +75,7 @@ function processmodel(pm)
     % Tools required: Simulink Design Verifier
     if includeDesignErrorDetectionTask
         dedTask = pm.addTask(padv.builtin.task.DetectDesignErrors(IterationQuery=findModels)); %#ok<*UNRCH>
+        dedTask.MaxProcessTime = 900;
         dedTask.ReportFilePath = fullfile( ...
             defaultResultPath, 'design_error_detections','$ITERATIONARTIFACT$_DED');
     end
