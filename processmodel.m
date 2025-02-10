2fd9842af429b5aa8e3859992e378a2b5b90e1f5
Ahmed Hassan <ahmedh@mathworks.com>
2025-02-10 00:39:08 -0500

    Disable includeDesignErrorDetectionTask

--- processmodel.m
+++ processmodel.m
@@ -12,7 +12,7 @@ function processmodel(pm)
     includeModelMaintainabilityMetricTask = true;
     includeModelTestingMetricTask = true;
     includeModelStandardsTask = true;
-    includeDesignErrorDetectionTask = true;
+    includeDesignErrorDetectionTask = false;
     includeFindClones = true;
     includeModelComparisonTask = true;
     includeSDDTask = true;
