1d7a7dd58743a160debe694053bbfab28e0bf3f9
Ahmed Hassan <ahmedh@mathworks.com>
2025-02-23 15:28:56 -0500

    gen mode

---
+++ generate_jenkins_pipeline.m
@@ -0,0 +1,15 @@
+% Copyright 2025 The MathWorks, Inc.
+
+function generate_jenkins_pipeline()
+    cp = openProject('scm');
+
+    op = padv.pipeline.JenkinsOptions;
+    op.PipelineArchitecture = "IndependentModelParallelJobs";
+    op.GeneratedPipelineDirectory = fullfile(cp.RootFolder, "pipelines", "derived");
+    op.MatlabInstallationLocation = "<MATLAB executable path>";
+    op.AgentLabel = "padv_win_agents";
+    op.StopOnStageFailure = true;
+    op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
+    op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
+    padv.pipeline.generatePipeline(op, "CIPipeline");
+end
