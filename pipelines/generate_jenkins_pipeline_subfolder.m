d0dd689b93dd6af4e9267f1e203ec4951b1a4455
Ahmed Hassan <ahmedh@mathworks.com>
2025-02-27 18:11:18 -0500

    using scm folder

--- pipelines/generate_jenkins_pipeline_subfolder.m
+++ pipelines/generate_jenkins_pipeline_subfolder.m
@@ -5,11 +5,10 @@ function generate_jenkins_pipeline_subfolder()
     % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
     op.PipelineArchitecture = "IndependentModelParallelJobs";
     op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
+    op.GeneratedPipelineDirectory = "scm";
     op.AgentLabel = "padv_win_agents";
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
     op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
     padv.pipeline.generatePipeline(op, "CIPipeline");
-    % Copy the pipeline and dag files on the project root folder
-    copyfile( fullfile(cp.RootFolder, 'derived', 'pipeline' , '*'), cp.RootFolder);
 end
