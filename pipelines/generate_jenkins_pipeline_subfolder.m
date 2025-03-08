f6d95b65d3a4efd9f4690ed4cfc86345da091fb0
Ahmed Hassan <ahmedh@mathworks.com>
2025-03-07 20:42:12 -0500

    Using old sandbox

--- pipelines/generate_jenkins_pipeline_subfolder.m
+++ pipelines/generate_jenkins_pipeline_subfolder.m
@@ -5,8 +5,8 @@ function generate_jenkins_pipeline_subfolder()
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
     op.PipelineArchitecture = "IndependentModelParallelJobs";
-    % op.MatlabInstallationLocation = "G:/86/ahmedh.Bslcicd.j2884790.10/matlab/bin";
-    op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
+    op.MatlabInstallationLocation = "G:/86/ahmedh.Bslcicd.j2884790.10/matlab/bin";
+    % op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
     % op.GeneratedPipelineDirectory = "";
     op.GeneratedPipelineDirectory = "scm";
     op.AgentLabel = "padv_win_agents";
