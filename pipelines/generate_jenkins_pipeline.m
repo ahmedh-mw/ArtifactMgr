51614d27de9741f7e665303b0c86b8e56c55d8fc
Ahmed Hassan <ahmedh@mathworks.com>
2025-03-08 09:51:39 -0500

    update sub folder settings

--- pipelines/generate_jenkins_pipeline.m
+++ pipelines/generate_jenkins_pipeline.m
@@ -1,15 +1,28 @@
-function generate_jenkins_pipeline()
+function generate_jenkins_pipeline_subfolder()
+    % ************** 1
+    % cp = openProject('scm');
+    % cp = openProject('scm');
+    % cp = openProject(pwd);
     cp = openProject('scm');
 
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
     op.PipelineArchitecture = "IndependentModelParallelJobs";
+
+    % ************** 2
+    % op.MatlabInstallationLocation = "G:/86/ahmedh.Bslcicd.j2884790.10/matlab/bin";
+    % op.MatlabInstallationLocation = "D:/sb/Bslcicd_0209/matlab";
+    % op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
     op.MatlabInstallationLocation = "D:/sb/r24b_0304/matlab/bin";
+
+    % ************** 3
+    % op.GeneratedPipelineDirectory = "";
+    % op.GeneratedPipelineDirectory = "";
+    % op.GeneratedPipelineDirectory = "scm";
+    op.GeneratedPipelineDirectory = "scm";
     op.AgentLabel = "padv_win_agents";
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
     op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
     padv.pipeline.generatePipeline(op, "CIPipeline");
-    % Copy the pipeline and dag files on the project root folder
-    copyfile( fullfile(cp.RootFolder, 'derived', 'pipeline' , '*'), cp.RootFolder);
 end
