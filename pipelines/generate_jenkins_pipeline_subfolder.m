de048299d8ed74065ead6071b6a82e28df74ffcc
Ahmed Hassan <ahmedh@mathworks.com>
2025-03-07 22:01:43 -0500

    old sandbox

--- pipelines/generate_jenkins_pipeline_subfolder.m
+++ pipelines/generate_jenkins_pipeline_subfolder.m
@@ -1,14 +1,19 @@
 function generate_jenkins_pipeline_subfolder()
-    % cp = openProject('scm');
-    cp = openProject(pwd);
+    % ************** 1
+    cp = openProject('scm');
+    % cp = openProject(pwd);
 
     op = padv.pipeline.JenkinsOptions;
     % op.PipelineArchitecture = "IndependentModelParallelJobs"; # SingleJob, SerialJobs, SerialJobsGroupPerTask
     op.PipelineArchitecture = "IndependentModelParallelJobs";
-    % op.MatlabInstallationLocation = "G:/86/ahmedh.Bslcicd.j2884790.10/matlab/bin";
-    op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
-    % op.GeneratedPipelineDirectory = "";
-    op.GeneratedPipelineDirectory = "scm";
+
+    % ************** 2
+    op.MatlabInstallationLocation = "G:/86/ahmedh.Bslcicd.j2884790.10/matlab/bin";
+    % op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
+
+    % ************** 3
+    op.GeneratedPipelineDirectory = "";
+    %op.GeneratedPipelineDirectory = "scm";
     op.AgentLabel = "padv_win_agents";
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
