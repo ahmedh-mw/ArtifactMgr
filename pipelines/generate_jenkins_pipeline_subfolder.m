7c5c998a7fb67fb4fa97ae7b20e24e0812356af2
Ahmed Hassan <ahmedh@mathworks.com>
2025-03-11 11:15:07 -0400

    test Linux

--- pipelines/generate_jenkins_pipeline_subfolder.m
+++ pipelines/generate_jenkins_pipeline_subfolder.m
@@ -10,14 +10,14 @@ function generate_jenkins_pipeline_subfolder()
 
     % ************** 2
     % op.MatlabInstallationLocation = "G:/86/ahmedh.Bslcicd.j2884790.10/matlab/bin";
-    op.MatlabInstallationLocation = "D:/sb/Bslcicd_0209/matlab";
+    op.MatlabInstallationLocation = "/mathworks/devel/sbs/86/ahmedh.Bslcicd.j2896530";
     % op.MatlabInstallationLocation = "D:/sb/Bslcicd_0225/matlab/bin";
 
     % ************** 3
     % op.GeneratedPipelineDirectory = "";
     op.GeneratedPipelineDirectory = "";
     %op.GeneratedPipelineDirectory = "scm";
-    op.AgentLabel = "padv_win_agents";
+    op.AgentLabel = "padv_linux_agents";
     op.StopOnStageFailure = true;
     op.RunprocessCommandOptions.GenerateJUnitForProcess = true;
     op.ReportPath = "$PROJECTROOT$/PA_Results/Report/ProcessAdvisorReport";
